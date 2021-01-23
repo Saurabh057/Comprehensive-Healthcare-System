d89c305e4b8e8b744fe4f72e9781d21caab0ac21
Saurabh Gore <saurabhgore38@gmail.com>
2021-01-23 10:03:58 +0530

    housrs ch add kelay

--- proj/context_processors.py
+++ proj/context_processors.py
@@ -5,6 +5,8 @@ from datetime import datetime
 def add_variable_to_context(request):
       if request.user.is_authenticated:
             info = AddtionalDetails.objects.get(username=request.user.username)
+
+   
             list1=[]
             for i in info.notifications:
                   j=i[2:-2]
@@ -14,9 +16,11 @@ def add_variable_to_context(request):
                   a=datetime.strptime(k[1],'%Y-%m-%d %H:%M:%S.%f')
                   diff=datetime.now()-a 
                   # k[1]=str((diff.seconds)//60)+"  minutes ago"
-
+                  hrs=(diff.seconds)//3600
                   if(diff.days==0):
-                        k[1]=str((diff.seconds)//60)+"  minutes ago"
+                        k[1]=str(hrs)+"  hours ago"
+                  elif hrs==0:
+                        k[1]=str(hrs//60)+"  minutes ago"
                   else:
                         k[1]=str(diff.days)+"  days ago"
 
