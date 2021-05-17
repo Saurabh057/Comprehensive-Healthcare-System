6a936359eff7198891d4b2d7af917cd6c1e548f2
RAJ <yorajpatil@github.com>
2021-05-17 21:08:34 +0530

    added inbuilt algos

--- diagnose/views.py
+++ diagnose/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from disease.paralleldt import dt
 from django.views.decorators.csrf import csrf_exempt
 import json
-
+import disease.inbuilt as inbuilt
 from disease.naivebayes import soln
 from disease.knn import knn
 from disease.decisiontree import decisiontree
@@ -64,6 +64,58 @@ def diangnose(request):
 
     return render(request, 'diagnose/diagnoseDash.html', context)
 
+def diangnose2(request):
+    symp=request.POST.get("symptoms")
+    s=symp.split(',')
+    sys=[]
+    i=0
+    while(i<len(s)):
+        if(s[i+1]=='1'):
+            sys.append(s[i])
+        i=i+2
+    print(sys)
+    nb=inbuilt.nb(sys)
+    print(nb)
+    accNaiveBayes = nb[0]
+    diseaseNaiveBAyes = nb[1] #yash bhadkahv ithe functions madhun return kr
+    
+    kn=inbuilt.kn(sys)
+    print(kn)
+    accKnn = kn[0]
+    diseaseKnn = kn[1]
+    
+    dt=inbuilt.dt(sys)
+    print(dt)
+    accDecisionTree = dt[0]
+    diseaseDecisionTree = dt[1]
+    
+    
+    
+    
+    if "prognosis" in sys:
+        sys.remove("prognosis")
+    symptoms = sys
+
+    symptoms = str(symptoms).replace('[','').replace(']','').replace("'",'')
+
+    disease = {
+        'NBS' : ['Naive Bayes Classifier',diseaseNaiveBAyes, accNaiveBayes],
+        'KNN' : ['K-Nearest Neighbors Algorithm',diseaseKnn, accKnn],
+        'DTC' : ['Decision Tree Classifier',diseaseDecisionTree, accDecisionTree]
+    }
+
+    choosenAlgorithm =  max(disease, key=lambda x : disease[x][2])
+    print(choosenAlgorithm)
+
+    chosen = {
+        'choosenAlgorithm' : disease[choosenAlgorithm][0],
+        'choosenDisease' : disease[choosenAlgorithm][1],
+        'chosenAcc' : disease[choosenAlgorithm][2]
+    }
+
+    context = {'chosen':chosen, 'disease': disease, 'symptoms':symptoms}
+
+    return render(request, 'diagnose/diagnoseDash.html', context)
 
 
 @csrf_exempt
--- disease/inbuilt.py
+++ disease/inbuilt.py
@@ -7,10 +7,11 @@ import operator
 
 def kn(inp):
     from sklearn.neighbors import KNeighborsClassifier
-    train=pd.read_csv('Training.csv',header=0)
+    train=pd.read_csv('disease/Training.csv',header=0)
     xtrain=train.iloc[:,:-1]
     ytrain=train.iloc[:,-1]
-    test=pd.read_csv('Testing.csv',header=0)
+    # xtrain,xtest,ytrain,ytest=train_test_split(xtrain,ytrain, test_size=0.3, random_state=100)
+    test=pd.read_csv('disease/Testing.csv',header=0)
     xtest=test.iloc[:,:-1]
     ytest=test.iloc[:,-1]
     # acc={}
@@ -18,7 +19,7 @@ def kn(inp):
     knn=KNeighborsClassifier(19)
     knn.fit(xtrain,ytrain)
     ypred=knn.predict(xtest)
-    acc=accuracy_score(ypred,ytest)
+    acc=int(accuracy_score(ypred,ytest)*100)
     print(acc)
 
     input=[]
@@ -28,9 +29,90 @@ def kn(inp):
         else:
             input.append(0)
     # input=input[:-1]
-    # print(xtest)
-    pred=knn.predict(input)
+    pred=knn.predict([input])
     print(pred)
+    return [acc,pred[0]]
 
-kn(["vomiting","nausea"])
+def nb(inp):
+    from sklearn.naive_bayes import GaussianNB
+    train=pd.read_csv('disease/Training.csv',header=0)
+    xtrain=train.iloc[:,:-1]
+    ytrain=train.iloc[:,-1]
+    # xtrain,xtest,ytrain,ytest=train_test_split(xtrain,ytrain, test_size=0.3, random_state=100)
+    test=pd.read_csv('disease/Testing.csv',header=0)
+    xtest=test.iloc[:,:-1]
+    ytest=test.iloc[:,-1]
+    gnb=GaussianNB()
+    gnb.fit(xtrain,ytrain)
+    ypred=gnb.predict(xtest)
+    acc=int(accuracy_score(ypred,ytest)*100)
+    print(acc)
+
+    input=[]
+    for i in xtrain.columns:
+        if(i in inp):
+            input.append(1)
+        else:
+            input.append(0)
+    # input=input[:-1]
+    pred=gnb.predict([input])
+    print(pred)
+    return [acc,pred[0]]
+
+def lr(inp):
+    from sklearn.linear_model import LogisticRegression
+    train=pd.read_csv('disease/Training.csv',header=0)
+    xtrain=train.iloc[:,:-1]
+    ytrain=train.iloc[:,-1]
+    # xtrain,xtest,ytrain,ytest=train_test_split(xtrain,ytrain, test_size=0.3, random_state=100)
+    test=pd.read_csv('disease/Testing.csv',header=0)
+    xtest=test.iloc[:,:-1]
+    ytest=test.iloc[:,-1]
+    log=LogisticRegression()
+    log.fit(xtrain,ytrain)
+    ypred=log.predict(xtest)
+    acc=int(accuracy_score(ypred,ytest)*100)
+    print(acc)
+
+    input=[]
+    for i in xtrain.columns:
+        if(i in inp):
+            input.append(1)
+        else:
+            input.append(0)
+    # input=input[:-1]
+    pred=log.predict([input])
+    print(pred)
+    return [acc,pred[0]]
+
+def dt(inp):
+    from sklearn.tree import DecisionTreeClassifier
+    train=pd.read_csv('disease/Training.csv',header=0)
+    xtrain=train.iloc[:,:-1]
+    ytrain=train.iloc[:,-1]
+    # xtrain,xtest,ytrain,ytest=train_test_split(xtrain,ytrain, test_size=0.3, random_state=100)
+    test=pd.read_csv('disease/Testing.csv',header=0)
+    xtest=test.iloc[:,:-1]
+    ytest=test.iloc[:,-1]
+    dtc=DecisionTreeClassifier(criterion = "gini", random_state = 100,max_depth=133, min_samples_leaf=5)
+    dtc.fit(xtrain,ytrain)
+    ypred=dtc.predict(xtest)
+    acc=int(accuracy_score(ypred,ytest)*100)
+    print(acc)
+
+    input=[]
+    for i in xtrain.columns:
+        if(i in inp):
+            input.append(1)
+        else:
+            input.append(0)
+    # input=input[:-1]
+    pred=dtc.predict([input])
+    print(pred)
+    return [acc,pred[0]]
+# lr(["vomiting","nausea"])
+# kn(["vomiting","nausea"])
+# nb(["vomiting","nausea"])
+# dt(["vomiting","nausea"])
+
 
