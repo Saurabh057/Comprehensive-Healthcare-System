5c8aa5fcf7f32f1e15cd0a3a95426fc178c42e3a
Abhijit <abhijitshendagescoe@gmail.com>
2021-05-19 11:36:18 +0530

    Calendar Added : Though links and other stuff remaining

--- calendr/urls.py
+++ calendr/urls.py
@@ -1,7 +1,7 @@
-"""project URL Configuration
+"""disa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import render
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('proj.urls')),
-    path('log/', include('log.urls')),
-    path('admin/', admin.site.urls),
-    path('diagnose/',include('diagnose.urls')),
-    path('calendar/',include('calendr.urls'))
+    path('',views.home, name="home"),
 ]
