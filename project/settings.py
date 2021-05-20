b69e042c7be505a32594660e75d57714e77e452e
Saurabh Gore <saurabhgore38@gmail.com>
2021-05-21 01:01:52 +0530

    homepage Done

--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,13 @@ STATIC_ROOT = os.path.join(BASE_DIR,'assests')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media') 
 MEDIA_URL = '/media/'
+
+
+#DataFlair
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'chs390118@gmail.com'
+EMAIL_HOST_PASSWORD = 'Mandakini@4'
